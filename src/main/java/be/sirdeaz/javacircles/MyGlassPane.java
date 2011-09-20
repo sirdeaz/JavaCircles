b3c1cc26f02e7c540afcd36e5594cfcf88124a39
sirdeaz <frederik.didden@gmail.com>
2011-09-20 23:11:38 +0200

    Pimped the drag&drop support.
    Added images to indicate an add and remove action.

--- src/main/java/be/sirdeaz/javacircles/MyGlassPane.java
+++ src/main/java/be/sirdeaz/javacircles/MyGlassPane.java
@@ -4,12 +4,18 @@
  */
 package be.sirdeaz.javacircles;
 
-import be.sirdeaz.ghostdragndrop.GhostGlassPane;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Point;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+
+import be.sirdeaz.ghostdragndrop.GhostGlassPane;
 
 /**
  *
@@ -19,18 +25,27 @@ import java.awt.Point;
 
     private static final long serialVersionUID = 1L;
     private Mode mode = Mode.NONE;
-
+    
+    private static Image addImage = loadImage("add.png");
+    private static Image deleteImage = loadImage("delete.png");
+    
+    public MyGlassPane() {
+		
+	}
+    
     private void drawMode(Graphics2D g2, int x, int y, int width, int height) {
         switch (this.mode) {
             case NONE:
                 break;
             case ADD:
                 g2.setPaint(Color.green);
-                g2.fillOval(x+width, y-25, width, height);
+                //g2.fillOval(x+width, y-25, width, height);
+                g2.drawImage(addImage, x+width-5, y+height/2, addImage.getWidth(null), addImage.getHeight(null), null);
                 break;
             case REMOVE:
                 g2.setPaint(Color.red);
-                g2.fillOval(x+width, y-25, width, height);
+                //g2.fillOval(x+width, y-25, width, height);
+                g2.drawImage(deleteImage, x+width-5, y+height/2, deleteImage.getWidth(null), deleteImage.getHeight(null), null);
                 break;
         }
     }
@@ -63,6 +78,19 @@ import java.awt.Point;
             null);
 
         drawMode(g2, x, y, dragged.getWidth(this), dragged.getHeight(this));
+    }
+    
+    private static Image loadImage(String name) {
+    	BufferedImage image = null;
+    	try {
+    		URL url = MyGlassPane.class.getResource(name);
+			image = ImageIO.read(url);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			throw new IllegalStateException(String.format("Image %s not found in classpath.", image));
+		}
+    	
+    	return image;
     }
 
 }
